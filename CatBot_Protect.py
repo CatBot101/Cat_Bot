5bb11f8f416838c34f7dd134d76bf20214df2ae4
Cat_Bot <33550285+CatBot101@users.noreply.github.com>
2017-11-10 22:24:27 +0700

    Update CatBot_Protect.py

--- CatBot_Protect.py
+++ CatBot_Protect.py
@@ -1122,8 +1122,9 @@ def bot(op):
 #--------------------------------------------------------
             elif msg.text in ["Sp","Speed","speed"]:
                 start = time.time()
-                cl.sendText(msg.to, "Progress...")
+                print("Speed")                
                 elapsed_time = time.time() - start
+		cl.sendText(msg.to, "Progress...")
                 cl.sendText(msg.to, "%sseconds" % (elapsed_time))
                 ki.sendText(msg.to, "%sseconds" % (elapsed_time))
                 kk.sendText(msg.to, "%sseconds" % (elapsed_time))
